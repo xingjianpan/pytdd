769d9025ce09e3163884e666c428deb32a832c3c
xingjianpan <xingjianpan@163.com>
2014-08-11 17:25:57 +0800

    changes

--- lists/tests.py
+++ lists/tests.py
@@ -61,8 +61,8 @@ class ItemModelTest(TestCase):
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
 
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
+        first_saved_item = saved_items[0].text
+        second_saved_item = saved_items[1].text
         self.assertEqual(first_saved_item, 'The first (ever) list item')
         self.assertEqual(second_saved_item, 'Item the second')
 
