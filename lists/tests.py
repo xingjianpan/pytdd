f8056c9c1315182ae2d53118011c615e81ec2508
xingjianpan <xingjianpan@163.com>
2014-08-11 13:50:42 +0800

    delete old test

--- lists/tests.py
+++ lists/tests.py
@@ -13,14 +13,6 @@ class SmokeTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
-
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        self.assertTrue(response.content.startswith(b'<html>'))
-        self.assertIn(b'<title>To-Do lists</title>', response.content)
-        self.assertTrue(response.content.strip().endswith(b'</html>'))
-
     def test_home_page_returns_correct_html_new(self):
         request = HttpRequest()
         response = home_page(request)
