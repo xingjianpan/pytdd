58b7f160d8d15aa1d7f8fa6c1036743d0ee7ce65
xingjianpan <xingjianpan@163.com>
2014-08-11 15:03:48 +0800

    fix an bug

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -53,10 +53,12 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
         table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_element_by_tag_name('tr')
+        rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
                 any(row.text == '1: Buy peacock feathers' for row in rows),
-                "New to-do item did not appear in table"
+            "New to-do item did not appear in table --its text was:\n%s" % (
+                table.text,
+            )
         )
 
         self.fail('Finish the test')
