e34551953bed5fec45e34bb0f5d712e7b54eacd2
xingjianpan <xingjianpan@163.com>
2014-08-15 18:09:01 +0800

    change URLs

--- superlists/urls.py
+++ superlists/urls.py
@@ -10,8 +10,6 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'lists.views.home_page', name='home'),
-    url(r'^lists/(\d+)/$',  'lists.views.view_list', name='view_list'),
-    url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
-    url(r'^lists/(\d+)/add_item$', 'lists.views.add_item', name='add_item'),
+    url(r'^lists/', include('lists.urls')),
 
 )
